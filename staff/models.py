2413cd9d574f6c84970e43cdfa828eb089c09ac4
Eloy Zuniga Jr. <eloyz.email@gmail.com>
2013-05-23 10:55:35 -0500

    Defaulting the StaffFile.position to 0 if NoneType object

--- staff/models.py
+++ staff/models.py
@@ -110,6 +110,7 @@ class StaffFile(OrderingBaseModel, File):
             # Append
             try:
                 last = StaffFile.objects.order_by('-position')[0]
+                last.position = last.position or 0
                 self.position = last.position + 1
             except IndexError:
                 # First row
